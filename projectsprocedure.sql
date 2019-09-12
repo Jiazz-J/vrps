5dbb4146956b6b4c9acb2cb7711010551a639b87
Jiazz-J <39787706+Jiazz-J@users.noreply.github.com>
2019-09-12 23:20:43 +0530

    Delete projectsprocedure.sql

--- projectsprocedure.sql
+++
@@ -1,6 +0,0 @@
-create or replace procedure insertall(empid in projects.empid%type,title in projects.title%type,startdate in projects.startdate%type,
-enddate in projects.enddate%type,description in projects.description%type) as
-begin
-insert into projects values(empid,title,startdate,enddate,description);
-end;
-/
